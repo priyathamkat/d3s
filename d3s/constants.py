b11d1ad5c7c2f5be7e5b34f9db16cb8a6d8c1652
priyathamkat <priyathamkat@gmail.com>
2022-09-22 16:20:27 -0400

    Add more constants

--- d3s/constants.py
+++ d3s/constants.py
@@ -1,5 +1,10 @@
 from pathlib import Path
 
-SALIENT_IMAGENET_PATH = Path("/fs/cml-datasets/Salient-ImageNet/")
 IMAGENET_PATH = Path("/fs/cml-datasets/ImageNet/ILSVRC2012/")
+SALIENT_IMAGENET_PATH = Path("/fs/cml-datasets/Salient-ImageNet/")
+SALIENT_IMAGENET_MASKS_PATH = SALIENT_IMAGENET_PATH / "salient_imagenet_dataset"
+MTURK_RESULTS_CSV_PATH = (
+    SALIENT_IMAGENET_PATH
+    / "salient_imagenet-main/mturk_results/discover_spurious_features.csv"
+)
 COCO_ROOT = Path("/fs/cml-datasets/coco/")
