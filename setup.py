019faa9f3cb7c223b0fd64237c9de0d66b48d81e
priyathamkat <priyathamkat@gmail.com>
2022-09-24 06:46:24 -0400

    Add `setup.py`

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="d3s",
+    version="0.1",
+    description="Diffusion Dreamed Distribution Shifts",
+    author="Priyatham Kattakinda",
+    author_email="pkattaki@umd.edu",
+    packages=find_packages(exclude=[".gitignore", "README.md", "LICENSE", "*.ipynb"]),
+    install_requires=[
+        "pyyaml",
+        "numpy",
+        "matplotlib",
+        "torch",
+        "torchvision",
+        "tqdm",
+        "Pillow",
+    ],
+)
