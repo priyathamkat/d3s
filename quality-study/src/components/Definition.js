ed2a7fc4ee1ba955afa2990f9a68a72ba67921a6
Priyatham Kattakinda <priyathamkat@gmail.com>
2022-09-28 15:00:55 -0400

    Remove starter code and reformat to 4 spaces

---
+++ quality-study/src/components/Definition.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class Definition extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    
+    render() {
+        return <div><i>a shuttlecock</i></div>;
+    }
+}
+
