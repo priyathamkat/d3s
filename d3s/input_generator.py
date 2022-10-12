ec67427e85ba1b072c3bfaafad08250d3f60f58c
priyathamkat <priyathamkat@gmail.com>
2022-10-12 16:10:20 -0400

    Extract `init_image` into a `dataclass`

--- d3s/input_generator.py
+++ d3s/input_generator.py
@@ -1,16 +1,53 @@
+from dataclasses import dataclass, field
 from pathlib import Path
 from string import Formatter, Template
-from typing import Any, Tuple, Union
+from typing import Any, Optional, Tuple, Union
 
 import numpy as np
+import torch
 import yaml
 from PIL import Image
 
 from d3s.datasets.coco import CocoDetection
 from d3s.datasets.salient_imagenet import SalientImageNet
-from d3s.utils import crop_to_squarize, paste_on_bg, resize
+from d3s.utils import crop_to_squarize, pad_to_squarize, paste_on_bg, resize
 
 rng = np.random.default_rng()
+
+
+@dataclass(repr=False, eq=False, frozen=True)
+class ImageInput:
+    fg_image: Optional[Image.Image] = None
+    bg_image: Optional[Image.Image] = None
+    mask: Optional[torch.Tensor] = None
+    fg_scale: float = 0.4
+    init_image: Image.Image = field(init=False)
+
+    def __post_init__(self):
+        mask = self.mask.numpy() if self.mask is not None else None
+        if self.fg_image is not None:
+            if self.bg_image is not None:
+                init_image = paste_on_bg(
+                    self.fg_image,
+                    self.bg_image,
+                    mask=mask,
+                    fg_scale=self.fg_scale,
+                )
+            else:
+                if self.mask is not None:
+                    init_image = pad_to_squarize(np.array(self.fg_image) * mask)
+                    init_image = resize(Image.fromarray(self.init_image), 512)
+                else:
+                    init_image = resize(crop_to_squarize(self.fg_image), 512)
+        else:
+            if self.bg_image is not None:
+                init_image = resize(crop_to_squarize(self.bg_image), 512)
+            else:
+                raise ValueError(
+                    "At least one of `fg_image` and `bg_image` must not be None."
+                )
+        # use __setattr__ to circumvent FrozenInstanceError
+        object.__setattr__(self, "init_image", init_image)
 
 
 class InputGenerator:
@@ -26,13 +63,16 @@ class InputGenerator:
 
         with open(Path(template_file).resolve(), "r") as f:
             # load template file
-            self.prompt_template = Template(
-                yaml.load(f, Loader=yaml.CLoader)["template"]
-            )
-            # parse all identifiers in the template
-            self.prompt_identifiers = {
+            template_file = yaml.load(f, Loader=yaml.CLoader)
+
+        self.prompt_templates = [Template(t) for t in template_file["template"]]
+        self.shift_name = template_file["shift_name"]
+        # parse all identifiers in the template
+        self.prompt_identifiers = {}
+        for prompt_template in self.prompt_templates:
+            self.prompt_identifiers[prompt_template] = {
                 e[1]
-                for e in Formatter().parse(self.prompt_template.template)
+                for e in Formatter().parse(prompt_template.template)
                 if e[1] is not None
             }
 
@@ -57,6 +97,8 @@ class InputGenerator:
             prompt (str): prompt input
             args (dict): arguments used to generate the prompt
         """
+        prompt_template = rng.choice(self.prompt_templates)
+        prompt_identifiers = self.prompt_identifiers[prompt_template]
         substitutes = {}
         # pick a random `class_idx` if not provided
         try:
@@ -67,16 +109,16 @@ class InputGenerator:
         finally:
             class_name = self.dataset.classes[class_idx]
             substitutes["class_name"] = class_name
-            if "definition" in self.prompt_identifiers:
+            if "definition" in prompt_identifiers:
                 substitutes["definition"] = self.dataset.dictionary[class_idx]
 
-        for identifier in self.prompt_identifiers - {"class_name", "definition"}:
+        for identifier in prompt_identifiers - {"class_name", "definition"}:
             try:
                 substitutes[identifier] = kwargs[identifier]
             except KeyError:
                 substitutes[identifier] = rng.choice(self.prompt_options[identifier])
                 kwargs[identifier] = substitutes[identifier]
-        return self.prompt_template.substitute(substitutes), kwargs
+        return prompt_template.substitute(substitutes), kwargs
 
     def generate_image(
         self,
@@ -102,7 +144,7 @@ class InputGenerator:
             not use_mask or use_foreground_image
         ), "Mask can only be used with foreground image"
 
-        fg = None
+        fg_image = None
         mask = None
         if use_foreground_image:
             try:
@@ -113,48 +155,46 @@ class InputGenerator:
             finally:
                 dataset_item = self.dataset.get_random(class_idx)
 
-            fg = dataset_item[0]
+            fg_image = dataset_item[0]
             if use_mask:
                 if isinstance(self.dataset, SalientImageNet):
-                    fg, mask = dataset_item
+                    fg_image, mask = dataset_item
                 elif (
                     isinstance(self.dataset, CocoDetection)
                     and self.dataset.catId is not None
                 ):
-                    fg, _, mask = dataset_item
+                    fg_image, _, mask = dataset_item
                 else:
                     raise ValueError(
                         f"{type(self.dataset).__name__} does not have masks"
                     )
 
-        bg = None
+        bg_image = None
         if use_background_image:
             try:
-                bg = kwargs["background"]
+                background = kwargs["background"]
             except KeyError:
-                bg = rng.choice(self.prompt_options["background"])
-                kwargs["background"] = bg
+                background = rng.choice(self.prompt_options["background"])
+                kwargs["background"] = background
             finally:
                 for background_folder in self.background_folders:
-                    if background_folder.name in bg:
-                        bg = rng.choice(list(background_folder.iterdir()))
+                    if background_folder.name in background:
+                        background_path = rng.choice(list(background_folder.iterdir()))
                         break
-            bg = Image.open(bg)
+            bg_image = Image.open(background_path)
 
-        if fg is None:
-            # Resize background image to 512x512
-            bg = crop_to_squarize(bg)
-            bg = resize(bg, 512)
-            return bg
-        elif bg is None:
-            # Resize foreground image to 512x512
-            fg = crop_to_squarize(fg)
-            fg = resize(fg, 512)
-            return fg
-        else:
-            if "fg_scale" not in kwargs:
-                kwargs["fg_scale"] = 0.4  # default `fg_scale`
-            return paste_on_bg(fg, bg, mask=mask, fg_scale=kwargs["fg_scale"])
+        if "fg_scale" not in kwargs:
+            kwargs["fg_scale"] = 0.4  # default `fg_scale`
+
+        return (
+            ImageInput(
+                fg_image=fg_image,
+                bg_image=bg_image,
+                mask=mask,
+                fg_scale=kwargs["fg_scale"],
+            ),
+            kwargs,
+        )
 
     def generate_input(
         self,
@@ -185,15 +225,15 @@ class InputGenerator:
         if use_background_image and "background" not in kwargs:
             kwargs["background"] = rng.choice(self.prompt_options["background"])
 
-        prompt, args = self.generate_prompt(**kwargs)
-        init_image = self.generate_image(
+        text_prompt, prompt_args = self.generate_prompt(**kwargs)
+        image_input, image_args = self.generate_image(
             use_foreground_image=use_foreground_image,
             use_background_image=use_background_image,
             use_mask=use_mask,
             **kwargs,
         )
-
-        return prompt, init_image, args
+        args = {**prompt_args, **image_args}
+        return text_prompt, image_input, args
 
 
 if __name__ == "__main__":
@@ -201,12 +241,13 @@ if __name__ == "__main__":
 
     input_generator = InputGenerator(
         ImageNet(),
-        "/cmlscratch/pkattaki/void/d3s/d3s/recipes/prompts/photo-definition-background.yaml",
+        Path(__file__).parent / "recipes/prompts/background-shift.yaml",
     )
-    prompt, image, _ = input_generator.generate_input(
+    text_prompt, image_input, _ = input_generator.generate_input(
         class_idx=100,
+        use_foreground_image=True,
         use_background_image=True,
         use_mask=False,
     )
-    print(prompt)
-    image.save("test.png")
+    print(text_prompt)
+    image_input.init_image.save("test.png")
