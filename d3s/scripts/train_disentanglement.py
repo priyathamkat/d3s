d678808bb786f868180d7174853aad7cf3be567d
priyathamkat <priyathamkat@gmail.com>
2022-11-04 07:32:00 -0400

    Use MINE for disentanglement

--- d3s/scripts/train_disentanglement.py
+++ d3s/scripts/train_disentanglement.py
@@ -3,7 +3,6 @@ from datetime import datetime
 from pathlib import Path
 
 import torch
-import torch.multiprocessing as mp
 import torch.nn as nn
 import torch.optim as optim
 import torchvision.models as models
@@ -13,9 +12,9 @@ from torch.utils.data import DataLoader
 from torch.utils.tensorboard import SummaryWriter
 from tqdm import tqdm, trange
 
-from d3s.contrastive_loss import ContrastiveLoss
 from d3s.datasets import D3S, ImageNet
 from d3s.disentangled_model import DisentangledModel
+from d3s.mine_loss import MINELoss
 
 FLAGS = flags.FLAGS
 
@@ -28,81 +27,126 @@ flags.DEFINE_bool("only_disentangle", True, "Only train disentanglement layer")
 flags.DEFINE_bool("lu_decompose", True, "Use LU decomposed linear layer")
 flags.DEFINE_integer("num_iters", 100000, "Number of iterations to train for")
 flags.DEFINE_integer("num_workers", 4, "Number of workers to use for data loading")
-flags.DEFINE_integer("train_batch_size", 4, "Batch size for training")
+flags.DEFINE_integer("train_batch_size", 32, "Batch size for training")
 flags.DEFINE_integer("val_batch_size", 64, "Batch size for validation")
-flags.DEFINE_float("lr", 1e-2, "Learning rate")
-flags.DEFINE_float("momentum", 0.9, "Momentum for SGD")
+flags.DEFINE_float("lr", 1e-3, "Learning rate")
 flags.DEFINE_float("weight_decay", 1e-4, "Momentum for SGD")
-flags.DEFINE_float("t", 0.3, "Temperature for InfoNCE")
+flags.DEFINE_float("mine_ma_rate", 0.1, "Moving average rate for MINE")
 flags.DEFINE_float("alpha", 0.1, "Weight for cross entropy loss")
-flags.DEFINE_integer("test_every", 5000, "Test every n iterations")
-flags.DEFINE_string(
-    "log_folder", None, "Path to log folder"
+flags.DEFINE_integer(
+    "switch_frequency", 10, "How often to switch between optimizing model and mine"
 )
+flags.DEFINE_integer("test_every", 5000, "Test every n iterations")
+flags.DEFINE_string("log_folder", None, "Path to log folder")
 
 
 class Trainer:
-    def __init__(self, model, t, alpha, only_disentangle=True) -> None:
+    def __init__(self, model, alpha=1.0, only_disentangle=True) -> None:
         self.model = model
         self.ce_criterion = nn.CrossEntropyLoss()
-        self.contrasive_criterion = ContrastiveLoss(t=t)
+        self.mine_criterions = {
+            "fg": MINELoss(
+                feature_dim=self.model.disentangle.num_features // 2,
+                ma_rate=FLAGS.mine_ma_rate,
+            ).cuda(),
+            "bg": MINELoss(
+                feature_dim=self.model.disentangle.num_features // 2,
+                ma_rate=FLAGS.mine_ma_rate,
+            ).cuda(),
+        }
         self.only_disentangle = only_disentangle
         if self.only_disentangle:
-            self.optimizer = optim.SGD(
+            self.model_optimizer = optim.SGD(
                 self.model.disentangle.parameters(),
                 lr=FLAGS.lr,
-                momentum=FLAGS.momentum,
                 weight_decay=FLAGS.weight_decay,
             )
             for name, parameter in self.model.named_parameters():
                 if "disentangle" not in name:
                     parameter.requires_grad = False
         else:
-            self.optimizer = optim.SGD(
+            self.model_optimizer = optim.SGD(
                 self.model.parameters(),
                 lr=FLAGS.lr,
-                momentum=FLAGS.momentum,
                 weight_decay=FLAGS.weight_decay,
             )
+        self._mine_optimizers = None
         self.scheduler = optim.lr_scheduler.ReduceLROnPlateau(
-            self.optimizer, patience=100, threshold=1e-3, cooldown=50, min_lr=1e-4
+            self.model_optimizer,
+            patience=500,
+            threshold=1e-3,
+            cooldown=100,
+            min_lr=1e-4,
         )
         self.alpha = alpha
 
-    def train(self, batch):
+    @property
+    def mine_optimizers(self):
+        if self._mine_optimizers is None:
+            self._mine_optimizers = {
+                "fg": optim.Adam(
+                    self.mine_criterions["fg"].parameters(),
+                    lr=FLAGS.lr,
+                ),
+                "bg": optim.Adam(
+                    self.mine_criterions["bg"].parameters(),
+                    lr=FLAGS.lr,
+                ),
+            }
+        return self._mine_optimizers
+
+    def reset_mine_optimizers(self):
+        self._mine_optimizers = None
+
+    def train(self, batch, optimize_mine):
         if self.only_disentangle:
             self.model.eval()
         else:
             self.model.train()
-        self.optimizer.zero_grad()
-        ce_loss = None
-        if not self.only_disentangle:
-            outputs, _, _ = self.model(batch["imagenet_images"])
-            ce_loss = self.ce_criterion(outputs, batch["imagenet_labels"])
-        contrastive_loss = 0
-        for quadruplet in batch["d3s_images"]:
-            _, query_fg, query_bg = self.model(quadruplet["query"])
-            _, same_class_fg, _ = self.model(quadruplet["same_class"])
-            _, same_bg_fg, same_bg_bg = self.model(quadruplet["same_bg"])
-            _, negatives_fg, negatives_bg = self.model(quadruplet["negatives"])
-            contrastive_loss += self.contrasive_criterion(
-                query_fg, same_class_fg, torch.concat([negatives_fg, same_bg_fg])
+
+        _, fg_features, bg_features = self.model(batch["d3s_images"])
+
+        if optimize_mine:
+            self.mine_optimizers["fg"].zero_grad()
+            loss = self.mine_criterions["fg"](
+                fg_features, batch["d3s_bg_labels"], optimize_T=True
             )
-            contrastive_loss += self.contrasive_criterion(
-                query_bg, same_bg_bg[0:], negatives_bg
+            loss.backward(retain_graph=True)
+            self.mine_optimizers["fg"].step()
+
+            self.mine_optimizers["bg"].zero_grad()
+            loss = self.mine_criterions["bg"](
+                bg_features, batch["d3s_fg_labels"], optimize_T=True
             )
-        contrastive_loss /= len(batch["d3s_images"])
-        if self.only_disentangle:
-            total_loss = contrastive_loss
+            loss.backward()
+            self.mine_optimizers["bg"].step()
         else:
-            total_loss = contrastive_loss + self.alpha * ce_loss
-        total_loss.backward()
-        self.optimizer.step()
-        self.scheduler.step(total_loss)
-        if self.only_disentangle:
-            return total_loss.item()
-        else:
-            return total_loss.item(), ce_loss.item(), contrastive_loss.item()
+            self.model_optimizer.zero_grad()
+            self.reset_mine_optimizers()
+
+            fg_mine_loss = self.mine_criterions["fg"](
+                fg_features, batch["d3s_bg_labels"], optimize_T=False
+            )
+            bg_mine_loss = self.mine_criterions["bg"](
+                bg_features, batch["d3s_fg_labels"], optimize_T=False
+            )
+            mine_loss = -(fg_mine_loss + bg_mine_loss)
+
+            if not self.only_disentangle:
+                outputs, _, _ = self.model(batch["imagenet_images"])
+                ce_loss = self.ce_criterion(outputs, batch["imagenet_labels"])
+                total_loss = mine_loss + self.alpha * ce_loss
+            else:
+                total_loss = mine_loss
+
+            total_loss.backward()
+            self.model_optimizer.step()
+            self.scheduler.step(total_loss)
+
+            if self.only_disentangle:
+                return total_loss.item()
+            else:
+                return total_loss.item(), ce_loss.item(), mine_loss.item()
 
 
 @torch.no_grad()
@@ -119,45 +163,6 @@ def test(model, dataloader, desc):
         top1 += (top1_indices == labels).sum().item()
         total += labels.shape[0]
     return 100 * top1 / total, 100 * top5 / total
-
-
-def get_from_d3s(input_queue, output_queue):
-    d3s = D3S(
-        Path(FLAGS.d3s_root),
-        split="train",
-        shift="background-shift",
-    )
-    while True:
-        label = input_queue.get()
-        if label is not None:
-            query, _, bg_idx = zip(*d3s.get_random(class_idx=label))
-            bg_idx = bg_idx[0]
-            same_class, _, _ = zip(*d3s.get_random(class_idx=label, num_samples=1))
-            same_bg, _, _ = zip(
-                *d3s.get_random(
-                    bg_idx=bg_idx,
-                    not_class_idx=label,
-                    num_samples=FLAGS.train_batch_size,
-                )
-            )
-            negatives, _, _ = zip(
-                *d3s.get_random(
-                    not_class_idx=label,
-                    not_bg_idx=bg_idx,
-                    num_samples=FLAGS.train_batch_size,
-                )
-            )
-
-            output_queue.put(
-                {
-                    "query": query,
-                    "same_class": same_class,
-                    "same_bg": same_bg,
-                    "negatives": negatives,
-                }
-            )
-        else:
-            break
 
 
 def main(argv):
@@ -178,6 +183,12 @@ def main(argv):
     if not FLAGS.only_disentangle:
         train_imagenet = ImageNet(split="train", transform=train_transform)
     val_imagenet = ImageNet(split="val", transform=val_transform)
+    train_d3s = D3S(
+        Path(FLAGS.d3s_root),
+        split="train",
+        shift="background-shift",
+        transform=train_transform,
+    )
     val_d3s = D3S(
         Path(FLAGS.d3s_root),
         split="val",
@@ -201,6 +212,14 @@ def main(argv):
         num_workers=FLAGS.num_workers,
         pin_memory=True,
     )
+    train_d3s_dataloader = DataLoader(
+        train_d3s,
+        batch_size=FLAGS.train_batch_size,
+        shuffle=True,
+        num_workers=FLAGS.num_workers,
+        pin_memory=True,
+    )
+    train_d3s_iter = iter(train_d3s_dataloader)
     val_d3s_dataloader = DataLoader(
         val_d3s,
         batch_size=FLAGS.val_batch_size,
@@ -209,9 +228,7 @@ def main(argv):
         pin_memory=True,
     )
 
-    trainer = Trainer(
-        model, FLAGS.t, FLAGS.alpha, only_disentangle=FLAGS.only_disentangle
-    )
+    trainer = Trainer(model, FLAGS.alpha, only_disentangle=FLAGS.only_disentangle)
 
     now = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
     log_folder = Path(FLAGS.log_folder) / now
@@ -220,83 +237,76 @@ def main(argv):
 
     writer = SummaryWriter(log_dir=log_folder)
 
-    input_queue = mp.Queue(maxsize=2 * FLAGS.num_workers)
-    output_queue = mp.Queue(maxsize=2 * FLAGS.num_workers)
-    processes = []
-    for _ in range(FLAGS.num_workers):
-        p = mp.Process(target=get_from_d3s, args=(input_queue, output_queue))
-        p.start()
-        processes.append(p)
+    optimize_mine = True
 
     for i in trange(1, FLAGS.num_iters + 1):
-        if FLAGS.only_disentangle:
-            batch = {}
-            labels = torch.randint(0, 1000, (FLAGS.train_batch_size,))
-        else:
+        try:
+            d3s_batch = next(train_d3s_iter)
+        except StopIteration:
+            train_d3s_iter = iter(train_d3s_dataloader)
+            d3s_batch = next(train_d3s_iter)
+
+        batch = {
+            "d3s_images": d3s_batch[0].cuda(),
+            "d3s_fg_labels": d3s_batch[1].cuda(),
+            "d3s_bg_labels": d3s_batch[2].cuda(),
+        }
+
+        if not FLAGS.only_disentangle:
             try:
-                images, labels = next(train_imagenet_iter)
+                imagenet_batch = next(train_imagenet_iter)
             except StopIteration:
                 train_imagenet_iter = iter(train_imagenet_dataloader)
-                images, labels = next(train_imagenet_iter)
+                imagenet_batch = next(train_imagenet_iter)
 
-            batch = {"imagenet_images": images.cuda(), "imagenet_labels": labels.cuda()}
-        batch["d3s_images"] = []
+            batch["imagenet_images"] = imagenet_batch[0].cuda()
+            batch["imagenet_labels"] = imagenet_batch[1].cuda()
 
-        for label in labels:
-            label = label.item()
-            input_queue.put(label)
-
-        for _ in labels:
-            d3s_batch = output_queue.get()
-            for k in d3s_batch:
-                # using `train_transform` inside `get_from_d3s` hangs for some reason
-                d3s_batch[k] = torch.stack(
-                    [train_transform(x) for x in d3s_batch[k]]
-                ).cuda()
-            batch["d3s_images"].append(d3s_batch)
-
+        if i % FLAGS.switch_frequency == 0:
+            optimize_mine = not optimize_mine
         if FLAGS.only_disentangle:
-            total_loss = trainer.train(batch)
+            total_loss = trainer.train(batch, optimize_mine=optimize_mine)
         else:
-            total_loss, ce_loss, contrastive_loss = trainer.train(batch)
+            total_loss, ce_loss, mine_loss = trainer.train(
+                batch, optimize_mine=optimize_mine
+            )
             writer.add_scalar("loss/ce_loss", ce_loss, i)
-            writer.add_scalar("loss/contrastive_loss", contrastive_loss, i)
-        writer.add_scalar("loss/total_loss", total_loss, i)
+            writer.add_scalar("loss/mine_loss", mine_loss, i)
+        if not optimize_mine:
+            writer.add_scalar("loss/total_loss", total_loss, i)
 
         if i % FLAGS.test_every == 0:
-            top1, top5 = test(
-                model,
-                val_imagenet_dataloader,
-                f"Testing on ImageNet after {i} iterations",
-            )
-            writer.add_scalar("imagenet/top1", top1, i)
-            writer.add_scalar("imagenet/top5", top5, i)
-            top1, top5 = test(
-                model, val_d3s_dataloader, f"Testing on D3S after {i} iterations"
-            )
-            writer.add_scalar("d3s/top1", top1, i)
-            writer.add_scalar("d3s/top5", top5, i)
+            if not FLAGS.only_disentangle:
+                top1, top5 = test(
+                    model,
+                    val_imagenet_dataloader,
+                    f"Testing on ImageNet after {i} iterations",
+                )
+                writer.add_scalar("imagenet/top1", top1, i)
+                writer.add_scalar("imagenet/top5", top5, i)
+                top1, top5 = test(
+                    model, val_d3s_dataloader, f"Testing on D3S after {i} iterations"
+                )
+                writer.add_scalar("d3s/top1", top1, i)
+                writer.add_scalar("d3s/top5", top5, i)
             torch.save(
                 {
                     "model": model.state_dict(),
-                    "optimizer": trainer.optimizer.state_dict(),
+                    "optimizer": trainer.model_optimizer.state_dict(),
                 },
                 log_folder / f"ckpt-{i}.pth",
             )
 
-    writer.add_custom_scalars(
-        {
-            "imagenet": {"imagenet": ["Multiline", ["imagenet/top1", "imagenet/top5"]]},
-            "d3s": {"d3s": ["Multiline", ["d3s/top1", "d3s/top5"]]},
-        }
-    )
+    if not FLAGS.only_disentangle:
+        writer.add_custom_scalars(
+            {
+                "imagenet": {
+                    "imagenet": ["Multiline", ["imagenet/top1", "imagenet/top5"]]
+                },
+                "d3s": {"d3s": ["Multiline", ["d3s/top1", "d3s/top5"]]},
+            }
+        )
     writer.close()
-
-    for _ in range(FLAGS.num_workers):
-        input_queue.put(None)  # sentinel values to signal subprocesses to exit
-
-    for p in processes:
-        p.join()  # wait for all subprocesses to finish
 
 
 if __name__ == "__main__":
