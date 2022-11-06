1b17f6c32f9c1ca0536ea18bd00451549e5477ff
priyathamkat <priyathamkat@gmail.com>
2022-11-06 03:53:33 -0500

    Fix `forward`

--- d3s/pretrained_models/clip_zero_shot_classifier.py
+++ d3s/pretrained_models/clip_zero_shot_classifier.py
@@ -2,21 +2,36 @@ import json
 from pathlib import Path
 
 import clip
+import torch
 import torch.nn as nn
+import torchvision.transforms as T
 
 
 class CLIPZeroShotClassifier(nn.Module):
-    def __init__(self):
+    def __init__(self, arch, device):
         super().__init__()
-        self.model, self.preprocess = clip.load("ViT-B/32")
+        arch = arch.replace("clip-", "")
+        self.model, preprocess = clip.load(arch, device=device)
+        self.transform = T.Compose(
+            [
+                T.Lambda(lambda x: preprocess(x)),
+                T.Lambda(lambda x: x.unsqueeze(0)),
+            ]
+        )
         with open(
             Path(__file__).parent.parent / "metadata/imagenet_classes.json", "r"
         ) as f:
             text = [v.split(",")[0] for v in json.load(f).values()]
-        tokens = clip.tokenize(text)
-        self.register_buffer("text_features", self.model.encode_text(tokens))
+        tokens = torch.cat([clip.tokenize(f"a photo of a {t}") for t in text]).to(
+            device
+        )
+        with torch.no_grad():
+            self.text_features = self.model.encode_text(tokens)
+            self.text_features /= self.text_features.norm(dim=-1, keepdim=True)
 
     def forward(self, x):
         image_features = self.model.encode_image(x)
-        image_logits, _ = self.model(image_features, self.text_features)
-        return image_logits
+        image_features /= image_features.norm(dim=-1, keepdim=True)
+        
+        similarity = (100.0 * image_features @ self.text_features.T).softmax(dim=-1)
+        return similarity
